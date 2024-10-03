cb82b117025d6935b2bbad2130bede1ceee6c0f3
Jonas <jonas.elias@icloud.com>
2024-10-03 21:51:33 +0200

    Used TypedForms instead of UntypedForms

--- src/app/body/project-request/project-request.component.ts
+++ src/app/body/project-request/project-request.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 import { DialogComponent } from '../../common/dialog/dialog.component';
+
+interface ContactForm {
+  name: FormControl<string | null>;
+  email: FormControl<string | null>;
+  message: FormControl<string | null>;
+}
 
 @Component({
   selector: 'app-project-request',
@@ -14,23 +20,23 @@ export class ProjectRequestComponent {
   private templateId = 'template_ffykjl8';
   private publicKey = 'RM-yBe0-jPUPgCD2Q'; // UserId
 
-  public contactForm!: FormGroup;
+  public contactForm!: FormGroup<ContactForm>;
   public disable: boolean = false;
 
   constructor(private fb: FormBuilder, public dialog: MatDialog) {}
 
   ngOnInit(): void {
-    this.contactForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      message: ['', Validators.required]
+    this.contactForm = this.fb.group<ContactForm>({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', Validators.required)
     });
   }
 
   validate() : boolean {
-    return this.contactForm.controls['name'].value !== '' &&
-    this.contactForm.controls['email'].value !== '' &&
-    this.contactForm.controls['message'].value !== '' &&
+    return this.contactForm.controls.name.value !== '' &&
+    this.contactForm.controls.email.value !== '' &&
+    this.contactForm.controls.message.value !== '' &&
     this.contactForm.valid;
   }
 
@@ -54,7 +60,7 @@ export class ProjectRequestComponent {
       emailjs.send(this.serviceId, this.templateId, templateParams, this.publicKey)
         .then((response: EmailJSResponseStatus) => {
           this.disable = false;
-          this.openDialog('message sent successfully', 'close');
+          this.openDialog('message sent successfully!', 'close');
         }, (error) => {
           this.disable = false;
           this.openDialog('Error: '+ error.status + ' message sent failed!' , 'close');
